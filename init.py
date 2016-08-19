c81d9c66614a70889a30a1a3f8fb53389ad19241
Darius Strasel <dstrasel@preventure.com>
2016-08-19 10:09:14 -0400

    Initial code commit

---
+++ init.py
@@ -0,0 +1,84 @@
+#=======================================================================================================================
+# IPND Stage 2 Final Project
+
+# You've built a Mad-Libs game with some help from Sean.
+# Now you'll work on your own game to practice your skills and demonstrate what you've learned.
+
+# For this project, you'll be building a Fill-in-the-Blanks quiz.
+# Your quiz will prompt a user with a paragraph containing several blanks.
+# The user should then be asked to fill in each blank appropriately to complete the paragraph.
+# This can be used as a study tool to help you remember important vocabulary!
+
+# Note: Your game will have to accept user input so, like the Mad Libs generator,
+# you won't be able to run it using Sublime's `Build` feature.
+# Instead you'll need to run the program in Terminal or IDLE.
+# Refer to Work Session 5 if you need a refresher on how to do this.
+
+# To help you get started, we've provided a sample paragraph that you can use when testing your code.
+# Your game should consist of 3 or more levels, so you should add your own paragraphs as well!
+
+#Todo: function which controls flow of game? 1.Introduction 2. Question set 3.Score/conclusion stub
+#Todo: score stub
+#-----> leaderboard?
+#Todo: question/answer stub
+#Todo: difficulty (with score modifier, and "try" count stub
+#Todo: lose/win stub
+#Todo: generate questions/answers lists
+#Todo:
+#=======================================================================================================================
+
+sample = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
+adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
+don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
+tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
+
+# The answer for ___1___ is 'function'. Can you figure out the others?
+
+# We've also given you a file called fill-in-the-blanks.pyc which is a working version of the project.
+# A .pyc file is a Python file that has been translated into "byte code".
+# This means the code will run the same as the original .py file, but when you open it
+# it won't look like Python code! But you can run it just like a regular Python file
+# to see how your code should behave.
+
+# Hint: It might help to think about how this project relates to the Mad Libs generator you built with Sean.
+# In the Mad Libs generator, you take a paragraph and replace all instances of NOUN and VERB.
+# How can you adapt that design to work with numbered blanks?
+
+# If you need help, you can sign up for a 1 on 1 coaching appointment: https://calendly.com/ipnd1-1/20min/
+
+#=======================================================================================================================
+# The following code is for the mad-lib generator:
+# Write code for the function play_game, which takes in as inputs parts_of_speech
+# (a list of acceptable replacement words) and ml_string (a string that
+# can contain replacement words that are found in parts_of_speech). Your play_game
+# function should return the joined list replaced, which will have the same structure
+# as ml_string, only that replacement words are swapped out with "corgi", since this
+# program cannot replace those words with user input.
+#=======================================================================================================================
+
+parts_of_speech = ["PLACE", "PERSON", "PLURALNOUN", "NOUN"]
+
+test_string = """This is PLACE, no NOUN named PERSON, We have so many PLURALNOUN around here."""
+
+
+def word_in_pos(word, parts_of_speech):
+    for pos in parts_of_speech:
+        if pos in word:
+            return pos
+    return None
+
+
+def play_game(ml_string, parts_of_speech):
+    output = 0
+    replaced = []
+    stringList = ml_string.split()
+    for i in stringList:
+        if word_in_pos(i, parts_of_speech) <> None:
+            replaced.append(i.replace(word_in_pos(i, parts_of_speech), "corgi"))
+            # replaced.append(i.replace(i, "corgi"))
+        else:
+            replaced.append(i)
+    return " ".join(replaced)
+
+
+print play_game(test_string, parts_of_speech)
